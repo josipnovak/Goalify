0c14af6750d4067c9bbbdf4259be2072e9f3bd02
josip.novak <josipnovak7@gmail.com>
2025-08-13 16:29:28 +0200

    Start of mobile design

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -54,23 +54,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _buttonClicked() {
+    print('Button clicked');
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // by the _buttonClicked method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
@@ -104,16 +95,23 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ButtonTheme(child: ElevatedButton(
+              onPressed: _buttonClicked,
+              child: const Text('Trivia'),
+            )),
+            ButtonTheme(child: ElevatedButton(
+              onPressed: _buttonClicked,
+              child: const Text('TicTacToe'),
+            )),
+            ButtonTheme(child: ElevatedButton(
+              onPressed: _buttonClicked,
+              child: const Text('HigherLower'),
+            )),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _buttonClicked,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
