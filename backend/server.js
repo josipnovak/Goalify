478e399bac1566d5dada580677f27948f4f9b21b
josip.novak <josipnovak7@gmail.com>
2025-08-20 20:56:23 +0200

    Changes on server

--- backend/server.js
+++ backend/server.js
@@ -155,7 +155,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running at: http://localhost:${PORT}`);
 });
 
